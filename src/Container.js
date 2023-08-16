f50adced0828baff91790100c5928b448ac7cd67
kaleikautakaoka <sachikaleikautakaoka@gmail.com>
2023-08-16 12:38:36 -0600

    about page, contact, updated, container.js added to import just one file in app.js

---
+++ src/Container.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import { capitalizeFirstLetter } from '../../utils/helpers';
+import classNames from 'classnames/bind';
+import index from './components/About/index';
+import index from './components/Contact/index';
